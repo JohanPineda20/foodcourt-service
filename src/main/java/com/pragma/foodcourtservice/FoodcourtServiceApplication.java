b5f2e76dfc2ca6d1f18da23911104c57aef4a9fd
Johan Pineda <mpineda348@gmail.com>
2023-09-18 20:05:22 -0500

    first commit: creating and configuring the project

---
+++ src/main/java/com/pragma/foodcourtservice/FoodcourtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.foodcourtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodcourtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodcourtServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/foodcourtservice/FoodcourtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.foodcourtservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodcourtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
