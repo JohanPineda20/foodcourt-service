7d0e00bba7f661490265d83f880c247589542b57
Johan Pineda <mpineda348@gmail.com>
2023-09-19 14:12:26 -0500

    api documented using sprindoc

--- src/main/java/com/pragma/foodcourtservice/infraestructure/documentation/OpenApiConfig.java
+++ src/main/java/com/pragma/foodcourtservice/infraestructure/documentation/OpenApiConfig.java
@@ -1,4 +1,34 @@
 package com.pragma.foodcourtservice.infraestructure.documentation;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.StringSchema;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
 public class OpenApiConfig {
+    @Bean
+    public OpenAPI openApiConfiguration() {
+
+        var exceptionSchema = new Schema<Map<String, String>>()
+                .addProperty("message", new StringSchema().example("User not found"));
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Microservice foodcourt hexagonal")
+                        .description("foodcourt management")
+                        .version("1.0.0")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org"))
+                        .termsOfService("http://swagger.io/terms/"))
+                .components(new Components()
+                        .addSchemas("Exception", exceptionSchema));
+    }
 }
--- src/main/java/com/pragma/foodcourtservice/infraestructure/input/rest/RestaurantController.java
+++ src/main/java/com/pragma/foodcourtservice/infraestructure/input/rest/RestaurantController.java
@@ -2,6 +2,11 @@ package com.pragma.foodcourtservice.infraestructure.input.rest;
 
 import com.pragma.foodcourtservice.application.dto.request.RestaurantRequest;
 import com.pragma.foodcourtservice.application.handler.IRestaurantHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +24,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestaurantController {
 
     private final IRestaurantHandler restaurantHandler;
+
+    @Operation(summary = "Add a new restaurant")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Restaurant created", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad request: wrong input data", content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Exception"))),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Exception"))),
+            @ApiResponse(responseCode = "409", description = "Conflict: Restaurant already exists, exception from feign request, user must be an owner, owner must have only one restaurant", content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Exception")))
+    })
     @PostMapping
     public ResponseEntity<Void> save(@Valid @RequestBody RestaurantRequest restaurantRequest){
         restaurantHandler.save(restaurantRequest);
